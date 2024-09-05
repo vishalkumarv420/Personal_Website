e86ba7c43e0f077279383ff2fae07229f350ef67
vishal kumar <vishalkumarb5644@gmail.com>
2024-09-05 22:56:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,31 +12,3 @@ document.getElementById('contact-form').addEventListener('submit', function(even
         emailError.textContent = ''; // Clear any previous error message
     }
 });
-
-// Typing animation effect
-
-    
-    type()document.addEventListener("DOMContentLoaded", function() {
-    const typingText = document.querySelector(".typing-text");
-    const text = "Hey I'm Vishal";
-    
-    typingText.style.width = text.length + "ch"; // Set the width based on the text length
-    typingText.style.animation = typing 3s steps(${text.length}), blink .75s step-end infinite;
-    
-    let i = 0;
-    function type() {
-        if (i < text.length) {
-            typingText.textContent += text.charAt(i);
-            i++;
-            setTimeout(type, 150);
-        }
-    };
-});document.querySelector('.nav-toggle').addEventListener('click', function() {
-    document.querySelector('.nav-menu').classList.toggle('show');
-});
-
-document.querySelectorAll('.nav-menu a').forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelector('.nav-menu').classList.remove('show');
-    });
-});
